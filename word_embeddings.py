8d255182c61abee76df522d2fc65dafd1cdad489
Zach Ferguson <ferguszo@dukes.jmu.edu>
2025-04-29 17:46:28 -0400

    Fix comments, saving and hyperparameters

--- word_embeddings.py
+++ word_embeddings.py
@@ -8,10 +8,10 @@ from datasets import load_dataset
 from collections import Counter
 
 import numpy as np
-from scipy import spatial
+from scipy import spatial # type: ignore
 
 CONTEXT_SIZE = 2        # The number of words before and after the context word
-EMBEDDING_DIM = 10      # The number of dimensions to track for dimensionality
+EMBEDDING_DIM = 128     # The number of dimensions to track for dimensionality
 BATCH_SIZE = 32         # The size to batch by
 
 DIR = "C:\\Users\\Zach\\Desktop\\HI BOYFRIEND\\Transformer"
@@ -61,25 +61,25 @@ class NGramLanguageModeler(nn.Module):
     def __init__(self, vocab_size, embedding_dim, context_size):
         super(NGramLanguageModeler, self).__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)         # Establish the embedding matrix (lookup table)
-        self.linear1 = nn.Linear((context_size * 2) * embedding_dim, embedding_dim) # Project large input vector into vector of embedding_dim
-        self.linear2 = nn.Linear(embedding_dim, vocab_size)                         # Map the projection to vector of size vocab
+        self.linear1 = nn.Linear((context_size * 2) * embedding_dim, 128) # Project large input vector into vector of embedding_dim
+        self.linear2 = nn.Linear(128, vocab_size)                         # Map the projection to vector of size vocab
     
     def forward(self, inputs):
         embeds = self.embeddings(inputs).view(inputs.shape[0], -1)  # Get vectors of the inputs and flatten using view
         h1 = self.linear1(embeds)                                   # Get first hidden layer (inputs -> hidden layer 1)
         r = F.relu(h1)                                              # Apply non-linearity (h1 -> relu)
         out = self.linear2(r)                                       # Pass through linear2 to get final layer
-        return F.log_softmax(out, dim=1)                            # Softmax the final layer to get the log probability
+        return F.log_softmax(out, dim=1)                            # Softmax the final layer to get the probability
 
 
 def find_most_similar(word, word_to_ix, embeddings, n=5):
     norm = embeddings / embeddings.norm(dim=1, keepdim=True)    # Normalize the embeddings
     word_embed = norm[word_to_ix[word]].unsqueeze(0)            # Get the word embedding and store in size (1, EMBEDDING_DIM)
-    cos_sim = torch.mm(word_embed, norm.t()).squeeze(0)         # Matrix multipy word embeddings and squeeze into (vocab_size,)
+    cos_sim = torch.mm(word_embed, norm.t()).squeeze(0)         # Matrix multiply word embeddings and squeeze into (vocab_size,)
     top_n = torch.topk(cos_sim, n + 1)                          # Get the k largest elements of the cosine similarities tensor
 
     similar_words = []
-    ix_to_word = {i: w for w, i in word_to_ix.items()}          # Convert the indicies back to the words
+    ix_to_word = {i: w for w, i in word_to_ix.items()}          # Convert the indices back to the words
     
     for idx in top_n.indices:       # Get the index of the top_n words
         w = ix_to_word[idx.item()]  # Get the word given the index
@@ -119,7 +119,7 @@ def main():
 
     torch.manual_seed(1)
 
-    ds = load_dataset("fancyzhx/ag_news")   # Using this simple lil dataset for now
+    ds = load_dataset("fancyzhx/ag_news")   # Using this simple "lil" dataset for now
     data_text = ds['train']['text']         # Get all of the text in the training set (disregard labels for now)
 
     ngrams, word_to_index, vocab = ngram_gen(data_text)
@@ -128,10 +128,11 @@ def main():
     model = NGramLanguageModeler(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE).to(device)    # Create the model to be used for Ngrams
     optimizer = optim.SGD(model.parameters(), lr=0.001)                                 # Using pytorch optimizer
 
-    for epoch in range(10):
-        print(epoch)
+    for epoch in range(1):
+        print(f'Epoch: {epoch + 1}')
 
         random.shuffle(ngrams)
+        
         for start in range(0, len(ngrams), BATCH_SIZE):
             batch = ngrams[start : start + BATCH_SIZE]
 
@@ -151,8 +152,8 @@ def main():
             loss = loss_function(log_probs, target_batch)
             loss.backward()
             optimizer.step()
-    
-    torch.save(model, PATH)
+            
+            torch.save(model, PATH) # Is this how to checkpoint? Save after every epoch
     
     self_sim = find_most_similar("oil", word_to_index, model.embeddings.weight, n=50)
     print(f'Words similar to "oil": {self_sim}')
@@ -167,7 +168,7 @@ def main():
         if item in glove_sim:
             count += 1
     
-    print(f'Total overlap beteween glove and ag-news dataset: {count}')
+    print(f'Total overlap between glove and ag-news dataset: {count}')
 
 
 if __name__ == "__main__":
