b7047f6e3e6c5fedc4b1df9271685a12309be7e6
Zach Ferguson <ferguszo@dukes.jmu.edu>
2025-09-13 07:09:06 -0400

    Changed C++ tokenization from bigrams to ngrams.

--- wrapper_config/c_tokenization.cpp
+++ wrapper_config/c_tokenization.cpp
@@ -9,13 +9,42 @@
 
 using namespace std;
 
+
+vector<pair<pair<string, string>, int>> get_frequency ( const vector<string>& tokens ) {
+    map<pair<string, string>, int> ngrams;
+
+    for(size_t i = 0; i < tokens.size() - 1; i++) {
+        pair<string, string> ngram = make_pair(tokens[i], tokens[i + 1]);
+        ngrams[ngram]++;
+    }
+
+    vector<pair<pair<string, string>, int>> ngram_vec(ngrams.begin(), ngrams.end());
+
+    sort(ngram_vec.begin(), ngram_vec.end(), [](const auto &a, const auto &b) {
+        return a.second > b.second;
+    });
+
+    return ngram_vec;
+}
+
+
+vector<string> split_text (const string& text) {
+    vector<string> tokens;
+    for (char c : text) {
+        string char_to_str = string(1, c);
+        tokens.push_back(char_to_str);
+    }
+    return tokens;
+}
+
+
 std::vector<std::string> tokenize(
     const std::string &vocab_str,               // List of all vocabulary recognized before (letters, punctuation, numbers)
     const std::string &text,                    // Text to tokenize and add to vocabulary
-    const int vocab_size,                       // Set a limit for maximum size of vocabulary
+    const size_t vocab_size,                       // Set a limit for maximum size of vocabulary
     const std::vector<std::string> &specials    // Vector holding special characters to be recognized
 ) {
-    std::vector<std::string> vocab;
+    vector<string> vocab;
 
     // Initialize the vocabulary with specials and alpha/punc/digits passed from python
     vocab.insert(vocab.end(), specials.begin(), specials.end());
@@ -23,25 +52,31 @@ std::vector<std::string> tokenize(
         vocab.push_back(std::string(1, c));
     }
 
-    map<string, int> freq_list;
+    // Tokenize the given text
+    vector<string> tokens = split_text(text);
+    
+    while (vocab.size() <= vocab_size) {
+        auto frequency = get_frequency(tokens);
+        if (frequency.empty()) break;
 
-    for(size_t i = 0; i < text.length() - 1; i++) {
-        std::string bigram = text.substr(i, 2);
-        freq_list[bigram]++;
-    }
+        pair<string, string> best_ngram = make_pair(frequency[0].first.first, frequency[0].first.second);
+        vocab.push_back(best_ngram.first + best_ngram.second);
 
-    vector<pair<string, int>> freq_vec (freq_list.begin(), freq_list.end());
+        vector<string> new_tokens;
+        size_t i = 0;
 
-    sort(freq_vec.begin(), freq_vec.end(), [](const pair<string, int> &a, const pair<string, int> &b){
-        return a.second > b.second;
-    });
-
-    for (auto &p : freq_vec) {
-        if (vocab.size() >= vocab_size) break;
-
-        if (find(vocab.begin(), vocab.end(), p.first) == vocab.end()) {
-            vocab.push_back(p.first);
+        while (i < tokens.size()) {
+            if (tokens[i] == best_ngram.first and tokens[i + 1] == best_ngram.second) {
+                new_tokens.push_back(best_ngram.first + best_ngram.second);
+                i += 2;
+            }
+            else {
+                new_tokens.push_back(tokens[i]);
+                i += 1;
+            }
         }
+
+        tokens = new_tokens;
     }
 
     return vocab;
